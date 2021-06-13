7edaede5d22fa121704fd445c9afa467741fda93
anuragdandge <41123540+anuragdandge@users.noreply.github.com>
2021-06-13 21:39:03 +0530

    add
    add

--- UserData.js
+++ UserData.js
@@ -21,19 +21,19 @@ function addItems(count, name, number, email, date) {
   var td2 = document.createElement("td");
   var td3 = document.createElement("td");
   var td4 = document.createElement("td");
-  var td5 = document.createElement("td");
+  // var td5 = document.createElement("td");
 
   td1.innerHTML = count;
   td2.innerHTML = name;
   td3.innerHTML = number;
-  td4.innerHTML = email;
-  td5.innerHTML = date;
+  td4.innerHTML = date;
+  // td5.innerHTML = date;
 
   trow.appendChild(td1);
   trow.appendChild(td2);
   trow.appendChild(td3);
   trow.appendChild(td4);
-  trow.appendChild(td5);
+  // trow.appendChild(td5);
 
   tbody.appendChild(trow);
 }
